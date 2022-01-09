5fb33e38630575687bee7e3b481ecd31b8afc412
alesmirnova <hardbitten96@gmail.com>
2022-01-09 16:13:08 +0300

    rename function

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,11 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome()
+function getUserName()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+
+    return $name;
 }
