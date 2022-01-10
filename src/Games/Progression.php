0153e6ec553d53077861beb59343d3eac1f8f96b
alesmirnova <hardbitten96@gmail.com>
2022-01-10 19:54:18 +0300

    add progression game

---
+++ src/Games/Progression.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Brain\Games\Progression;
+
+use function Brain\Games\Engine\runGame;
+
+const TASK_DESCRIPTION = 'What number is missing in the progression?';
+const PROGRESSION_LENGTH = 10;
+
+function startProgressionGame()
+{
+    runGame(TASK_DESCRIPTION, getTaskAndAnswer());
+}
+
+function getProgressionArray(): array
+{
+    $startNumber = rand(1, 20);
+    $progressionStep = rand(1, 10);
+    $result = [$startNumber];
+
+    while (count($result) < PROGRESSION_LENGTH) {
+        $result[] = $result[count($result) - 1] + $progressionStep;
+    }
+
+    return $result;
+}
+
+function getTaskAndAnswer(): \Closure
+{
+    return function () {
+        $progression = getProgressionArray();
+        $randomNumber = rand(0, count($progression) - 1);
+        $progressionItem = $progression[$randomNumber];
+        $progression[$randomNumber] = '..';
+
+        $result[] = implode(' ', $progression);
+        $result[] = (string) $progressionItem;
+
+        return $result;
+    };
+}
