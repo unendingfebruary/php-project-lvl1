a994d36af95683b86179eafb1999594a5f6f1380
absque96 <41897970+absque96@users.noreply.github.com>
2022-01-15 19:03:09 +0300

    Update Prime.php

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -13,8 +13,12 @@ function startPrimeGame()
 
 function isPrime(int $number)
 {
+    if ($number < 2) {
+        return false;
+    }
+    
     for ($i = 2; $i <= sqrt($number); $i++) {
-        if ($number % $i == 0) {
+        if ($number % $i === 0) {
             return false;
         }
     }
